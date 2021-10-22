b9c8e2a2fc8718307eff21741d98d8a83a8f500a
Victor Paiva <victor.paivafgv@gmail.com>
2021-10-22 05:11:52 -0300

    Project created

---
+++ src/test/java/com/paiva/sbmongo/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paiva.sbmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
